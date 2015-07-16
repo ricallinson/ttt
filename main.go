26ad423fec0430521787286d153d2f730918c0f8
Ric Allinson <capecodehq@yahoo.co.uk>
2015-07-16 12:53:43 -0700

    Switched to precompiled regex

--- main.go
+++ main.go
@@ -5,32 +5,49 @@ import (
 	"regexp"
 )
 
+const(
+	X = 'x'
+	O = 'o'
+)
+
 type Game struct {
 	board      []byte
 	lastPlayer uint8
 }
 
-var wins = map[uint8][]string{
-	'x': []string{
-		"xxx......",
-		"...xxx...",
-		"......xxx",
-		"x..x..x..",
-		".x..x..x.",
-		"..x..x..x",
-		"x...x...x",
-		"..x.x.x..",
-	},
-	'o': []string{
-		"ooo......",
-		"...ooo...",
-		"......ooo",
-		"o..o..o..",
-		".o..o..o.",
-		"..o..o..o",
-		"o...o...o",
-		"..o.o.o..",
-	},
+var win = map[uint8][]*regexp.Regexp{}
+
+func init() {
+	checks := map[uint8][]string{
+		X: []string{
+			"xxx......",
+			"...xxx...",
+			"......xxx",
+			"x..x..x..",
+			".x..x..x.",
+			"..x..x..x",
+			"x...x...x",
+			"..x.x.x..",
+		},
+		O: []string{
+			"ooo......",
+			"...ooo...",
+			"......ooo",
+			"o..o..o..",
+			".o..o..o.",
+			"..o..o..o",
+			"o...o...o",
+			"..o.o.o..",
+		},
+	}
+	for _, w := range checks[X] {
+		regex, _ := regexp.Compile(w)
+		win[X] = append(win[X], regex)
+	}
+	for _, w := range checks[O] {
+		regex, _ := regexp.Compile(w)
+		win[O] = append(win[O], regex)
+	}
 }
 
 func CreateGame() *Game {
@@ -44,7 +61,7 @@ func (this *Game) Place(player uint8, pos int) (bool, error) {
 	if this.board[pos] != ' ' {
 		return false, errors.New("Position taken")
 	}
-	if player != 'x' && player != 'o' {
+	if player != X && player != O {
 		return false, errors.New("Not an 'x' or 'o'")
 	}
 	if player == this.lastPlayer {
@@ -52,9 +69,8 @@ func (this *Game) Place(player uint8, pos int) (bool, error) {
 	}
 	this.lastPlayer = player
 	this.board[pos] = player
-	for _, w := range wins[player] {
-		ok, _ := regexp.Match(w, this.board)
-		if ok {
+	for _, w := range win[player] {
+		if w.Match(this.board) {
 			return true, nil
 		}
 	}
@@ -72,7 +88,6 @@ func (this *Game) Move(player uint8, x int, y int) (bool, error) {
 	case 3:
 		pos = x + y + 2
 	}
-	// log.Printf("Player: %s, x: %d, y: %d, pos: %d\n", string(player), x, y, pos)
 	if pos < 0 || pos > 8 {
 		return false, errors.New("Bad position")
 	}
@@ -84,8 +99,8 @@ func (this *Game) Board() string {
 }
 
 func (this *Game) Player() uint8 {
-	if this.lastPlayer == 'x' {
-		return 'o'
+	if this.lastPlayer == X {
+		return O
 	}
-	return 'x'
+	return X
 }
